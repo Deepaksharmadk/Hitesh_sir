fc689672c41b485d98c30d9310278641beda067f
deepaksharmadk <dks847203@gmail.com>
2024-03-11 23:30:53 +0530

    first time deploy

---
+++ backend/index.js
@@ -0,0 +1,14 @@
+require("dotenv").config();
+const express = require("express");
+const app = express();
+console.log(app);
+console.log(process.env);
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+app.get("/twitter", (req, res) => {
+  res.send("Hello Deepak sharma");
+});
+app.listen(process.env.PORT, () => {
+  console.log(`Example app listening on port ${PORT}`);
+});
